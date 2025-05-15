d4949fd7bf6cb03259c78594b66e82f28f49cee9
jaspreet kaur <kaur1646@algonquinlive.com>
2025-05-15 20:33:07 +0530

    changes

---
+++ scripts/scripts.js
@@ -0,0 +1,29 @@
+//console.log will output a message in the developer console
+//two slashes starts a single line comment
+
+console.log("it worked"); //single line comments can go beside code
+// this is a multi-line comment
+// starts with a / and a* and closes with a* and a /
+
+// to declare a variable , use let or const
+let ourClassNum = "J207";
+let numStudents = 9; //with a number,you don't need quotes
+
+
+console.log(ourClassNum);
+
+
+//if you declare a variable with let,you can change its value
+numStudents = 21;
+
+console.log(numStudents);
+
+//if you declare a value with const, you can't change its value
+//ourclassnum= "C128"
+//trying to change a const variables will gave you an error, and stop the code)
+// ourClassNum = "C128";
+
+console.log(ourClassNum);
+
+//const and let cannot be redeclared (or recreated)
+// let numStudents = 25; (this will also give you an error)
